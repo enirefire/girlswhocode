5d161d7a57889c886cfefae7f63a3214f88a2f60
unknown <enirefire@gmail.com>
2019-07-18 15:27:38 -0400

    hi

---
+++ functionchallege.py
@@ -0,0 +1,27 @@
+print("hello")
+
+
+
+def oddandeven(answer):
+
+
+    if answer % 2 == 0:
+        return True
+    else:
+        return False
+
+answer=input("Put a number to see if it is even or odd.")
+answer= int(answer)
+print(oddandeven(answer))
+
+
+
+
+
+
+
+def calc_total(list):
+    for num in list:
+        sum += num
+    return calc_total
+print(calc_total[1,2,3,4,5])
